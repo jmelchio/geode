991426120d980529e7dc2f35a352b80fa1388d72
Dale Emery <demery@pivotal.io>
2020-04-02 17:45:11 -0700

    Improve existing OAuth redirect tests
    Split the existing test into two:
    1. When a user visits the login page, Spring redirects to the "client
       registration" URL that it generated for our ClientRegistration.
       This feels like a Spring implementation detail, but I'm not sure how
       to skip validating this.
    2. When ther browser hits that generated URL, Spring then redirects to
       the authorization URL that we defined for the client.
    
    Enhanced the second test to ensure that we have configured Spring to
    provide the right query parameters when it redirects to the
    authorization URI.
    
    Authored-by: Dale Emery <demery@pivotal.io>

--- geode-pulse/src/integrationTest/java/org/apache/geode/tools/pulse/security/OAuthSecurityConfigTest.java
+++ geode-pulse/src/integrationTest/java/org/apache/geode/tools/pulse/security/OAuthSecurityConfigTest.java
@@ -15,11 +15,15 @@
 
 package org.apache.geode.tools.pulse.security;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.web.util.UriComponentsBuilder.fromUriString;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -30,6 +34,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -38,6 +43,16 @@ import org.springframework.web.context.WebApplicationContext;
 @ContextConfiguration(locations = {"classpath*:WEB-INF/pulse-servlet.xml"})
 @ActiveProfiles({"pulse.authentication.oauth"})
 public class OAuthSecurityConfigTest {
+  // Defined by Spring because we registered an OAuth client with providerID=uaa
+  private static final String SPRING_OAUTH_CLIENT_URL_FOR_UAA =
+      "http://localhost/oauth2/authorization/uaa";
+  // The URI we specified for authorizing via UAA
+  private static final String PULSE_OAUTH_AUTHORIZATION_URL =
+      "http://example.com/uaa/oauth/authorize";
+  // Built from our redirectUrlTemplate: {baseUrl}/login/oauth2/code/{registrationId}
+  // with the registrationId filled in from our providerId (uaa).
+  private static final String PULSE_UAA_REDIRECT_URL = "http://localhost/login/oauth2/code/uaa";
+
   @Autowired
   private WebApplicationContext context;
 
@@ -51,13 +66,49 @@ public class OAuthSecurityConfigTest {
         .build();
   }
 
+  // TODO: Only from login page? Or from any page?
   @Test
-  public void redirectToOauth() throws Exception {
-    mvc.perform(get("/login.html")).andExpect(status().is3xxRedirection())
-        .andExpect(redirectedUrl(("http://localhost/oauth2/authorization/uaa")));
-
-    mvc.perform(get("http://localhost/oauth2/authorization/uaa"))
+  public void loginPageRedirectsUnauthenticatedUserToOAuthClientURL() throws Exception {
+    mvc.perform(get("/login.html"))
         .andExpect(status().is3xxRedirection())
-        .andExpect(redirectedUrlPattern("http://example.com/uaa/oauth/**"));
+        .andExpect(redirectedUrl(SPRING_OAUTH_CLIENT_URL_FOR_UAA));
+  }
+
+  @Test
+  public void oauthClientURLRedirectsToOAuthServerAuthorizationURL() throws Exception {
+    Map<String, String> expectedRedirectParams = new HashMap<>();
+    expectedRedirectParams.put("response_type", "code");
+    expectedRedirectParams.put("client_id", "pulse");
+    expectedRedirectParams.put("redirect_uri", PULSE_UAA_REDIRECT_URL);
+
+    mvc.perform(get(SPRING_OAUTH_CLIENT_URL_FOR_UAA))
+        .andExpect(status().is3xxRedirection())
+        .andExpect(redirectedUrlPath(PULSE_OAUTH_AUTHORIZATION_URL))
+        .andExpect(redirectedUrlParams(expectedRedirectParams));
+  }
+
+  private static ResultMatcher redirectedUrlPath(String expectedPath) {
+    return result -> {
+      String redirectedUrlString = result.getResponse().getRedirectedUrl();
+      assertThat(redirectedUrlString).isNotNull();
+      String actualPath = redirectedUrlString.split("[?#]")[0];
+      assertThat(actualPath)
+          .as("redirect URL path")
+          .startsWith(expectedPath);
+    };
+  }
+
+  private static ResultMatcher redirectedUrlParams(Map<String, String> expectedParams) {
+    return result -> {
+      String redirectedUrlString = result.getResponse().getRedirectedUrl();
+      assertThat(redirectedUrlString).isNotNull();
+      Map<String, String> actualParams = fromUriString(redirectedUrlString)
+          .build()
+          .getQueryParams()
+          .toSingleValueMap();
+      assertThat(actualParams)
+          .as("redirected URL params")
+          .containsAllEntriesOf(expectedParams);
+    };
   }
 }
