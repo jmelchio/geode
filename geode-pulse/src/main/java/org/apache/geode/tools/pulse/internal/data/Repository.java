338d0d23d3c19f800e1b51e585b5613d67506d4c
Dale Emery <demery@pivotal.io>
2020-04-16 17:12:53 -0700

    Refactor the expiration/refresh code
    Co-authored-by: Dale Emery <demery@pivotal.io>
    Co-authored-by: Joris Melchior <joris.melchior@gmail.com>

--- geode-pulse/src/main/java/org/apache/geode/tools/pulse/internal/data/Repository.java
+++ geode-pulse/src/main/java/org/apache/geode/tools/pulse/internal/data/Repository.java
@@ -17,6 +17,8 @@
 
 package org.apache.geode.tools.pulse.internal.data;
 
+import static java.time.Instant.now;
+
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -36,10 +38,9 @@ import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.client.endpoint.DefaultRefreshTokenTokenResponseClient;
 import org.springframework.security.oauth2.client.endpoint.OAuth2RefreshTokenGrantRequest;
-import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.AbstractOAuth2Token;
 import org.springframework.security.oauth2.core.OAuth2RefreshToken;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AccessTokenResponse;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Component;
 
 /**
@@ -50,6 +51,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class Repository {
   private static final Logger logger = LogManager.getLogger();
+  private static final Locale LOCALE =
+      new Locale(PulseConstants.APPLICATION_LANGUAGE, PulseConstants.APPLICATION_COUNTRY);
 
   private final OAuth2AuthorizedClientService authorizedClientService;
   private final ClusterFactory clusterFactory;
@@ -61,11 +64,9 @@ public class Repository {
   private boolean useSSLManager = false;
   private Properties javaSslProperties;
 
-  Locale locale =
-      new Locale(PulseConstants.APPLICATION_LANGUAGE, PulseConstants.APPLICATION_COUNTRY);
 
   private final ResourceBundle resourceBundle =
-      ResourceBundle.getBundle(PulseConstants.LOG_MESSAGES_FILE, locale);
+      ResourceBundle.getBundle(PulseConstants.LOG_MESSAGES_FILE, LOCALE);
 
   private final PulseConfig pulseConfig = new PulseConfig();
 
@@ -85,8 +86,101 @@ public class Repository {
     this.clusterFactory = clusterFactory;
   }
 
+  /**
+   * this will return a cluster already connected to the geode jmx manager for the user in the
+   * request
+   *
+   * But for multi-user connections to gemfireJMX, i.e pulse that uses gemfire integrated security,
+   * we will need to get the username from the context
+   */
+  public Cluster getCluster() {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    if (authentication == null) {
+      return null;
+    }
+
+    if (authentication instanceof OAuth2AuthenticationToken) {
+      return getClusterWithAuthenticationToken((OAuth2AuthenticationToken) authentication);
+    }
+
+    return getClusterWithUserNameAndPassword(authentication.getName(), null);
+  }
+
+  public Cluster getClusterWithUserNameAndPassword(String userName, String password) {
+    return getClusterWithCredentials(userName, new String[] {userName, password});
+  }
+
+  public Cluster getClusterWithCredentials(String username, Object credentials) {
+    synchronized (clusterMap) {
+      Cluster cluster = clusterMap.get(username);
+      if (cluster == null) {
+        logger.info(resourceBundle.getString("LOG_MSG_CREATE_NEW_THREAD") + " : " + username);
+        cluster = clusterFactory.create(host, port, username, resourceBundle, this);
+        // Assign name to thread created
+        cluster.setName(PulseConstants.APP_NAME + "-" + host + ":" + port + ":" + username);
+        cluster.connectToGemFire(credentials);
+        if (cluster.isConnectedFlag()) {
+          clusterMap.put(username, cluster);
+        }
+      }
+      return cluster;
+    }
+  }
+
+  private Cluster getClusterWithAuthenticationToken(OAuth2AuthenticationToken authentication) {
+    OAuth2AuthorizedClient authorizedClient = getAuthorizedClient(authentication);
+
+    if (!isExpired(authorizedClient.getAccessToken())) {
+      return getClusterWithAuthorizedClient(authorizedClient);
+    }
+
+    OAuth2AuthorizedClient freshClient = handleExpiration(authentication, authorizedClient);
+
+    if (freshClient == null) {
+      return null;
+    }
+
+    synchronized (clusterMap) {
+      Cluster cluster = clusterMap.get(freshClient.getPrincipalName());
+      if (cluster != null) {
+        cluster.reconnectToGemFire(freshClient.getAccessToken().getTokenValue());
+      }
+      return cluster;
+    }
+  }
+
+  private Cluster getClusterWithAuthorizedClient(OAuth2AuthorizedClient authorizedClient) {
+    return getClusterWithCredentials(authorizedClient.getPrincipalName(),
+        authorizedClient.getAccessToken().getTokenValue());
+  }
+
+  public void logoutUser(String username) {
+    Cluster cluster = clusterMap.remove(username);
+    if (cluster != null) {
+      try {
+        cluster.setStopUpdates(true);
+        cluster.getJMXConnector().close();
+      } catch (Exception e) {
+        // We're logging out so this can be ignored
+      }
+    }
+  }
+
+  public void removeAllClusters() {
+    Iterator<Map.Entry<String, Cluster>> iter = clusterMap.entrySet().iterator();
+
+    while (iter.hasNext()) {
+      Map.Entry<String, Cluster> entry = iter.next();
+      Cluster c = entry.getValue();
+      String clusterKey = entry.getKey();
+      c.stopThread();
+      iter.remove();
+      logger.info("{} : {}", resourceBundle.getString("LOG_MSG_REMOVE_THREAD"), clusterKey);
+    }
+  }
+
   public Boolean getJmxUseLocator() {
-    return this.jmxUseLocator;
+    return jmxUseLocator;
   }
 
   public void setJmxUseLocator(Boolean jmxUseLocator) {
@@ -95,19 +189,19 @@ public class Repository {
   }
 
   public String getHost() {
-    return this.host;
+    return host;
   }
 
   public void setHost(String jmxHost) {
-    this.host = jmxHost;
+    host = jmxHost;
   }
 
   public String getPort() {
-    return this.port;
+    return port;
   }
 
   public void setPort(String jmxPort) {
-    this.port = jmxPort;
+    port = jmxPort;
   }
 
   public boolean isUseSSLLocator() {
@@ -127,7 +221,7 @@ public class Repository {
   }
 
   public PulseConfig getPulseConfig() {
-    return this.pulseConfig;
+    return pulseConfig;
   }
 
   public Properties getJavaSslProperties() {
@@ -138,109 +232,60 @@ public class Repository {
     this.javaSslProperties = javaSslProperties;
   }
 
-  /**
-   * this will return a cluster already connected to the geode jmx manager for the user in the
-   * request
-   *
-   * But for multi-user connections to gemfireJMX, i.e pulse that uses gemfire integrated security,
-   * we will need to get the username from the context
-   */
-  public Cluster getCluster() {
-    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-    if (authentication == null) {
+  public ResourceBundle getResourceBundle() {
+    return resourceBundle;
+  }
+
+  private OAuth2AuthorizedClient handleExpiration(Authentication authentication,
+      OAuth2AuthorizedClient staleClient) {
+    OAuth2RefreshToken refreshToken = staleClient.getRefreshToken();
+
+    // If the refresh token is missing or expired, invalidate the current user's authentication
+    // and remove the user's cluster from the repository.
+    if (refreshToken == null || isExpired(refreshToken)) {
+      logoutUser(staleClient.getPrincipalName());
+      authentication.setAuthenticated(false);
       return null;
     }
 
-    if (authentication instanceof OAuth2AuthenticationToken) {
-      OAuth2AuthenticationToken authenticationToken = (OAuth2AuthenticationToken) authentication;
-      OAuth2AuthorizedClient authorizedClient = authorizedClientService.loadAuthorizedClient(
-          authenticationToken.getAuthorizedClientRegistrationId(),
-          authenticationToken.getName());
+    OAuth2AuthorizedClient freshClient = refreshClient(staleClient);
+    authorizedClientService.saveAuthorizedClient(freshClient, authentication);
 
-      OAuth2User authenticatedPrincipal = authenticationToken.getPrincipal();
-      String authenticatedPrincipalName = authenticatedPrincipal.getName();
-      OAuth2AccessToken accessToken = authorizedClient.getAccessToken();
-      if (Objects.requireNonNull(accessToken.getExpiresAt()).isBefore(Instant.now())) {
-        // need to refresh the access token
-        DefaultRefreshTokenTokenResponseClient responseClient =
-            new DefaultRefreshTokenTokenResponseClient();
-        OAuth2RefreshToken refreshToken = authorizedClient.getRefreshToken();
-        OAuth2RefreshTokenGrantRequest oAuth2RefreshTokenGrantRequest =
-            new OAuth2RefreshTokenGrantRequest(authorizedClient.getClientRegistration(),
-                accessToken,
-                Objects.requireNonNull(refreshToken));
-        OAuth2AccessTokenResponse tokenResponse =
-            responseClient.getTokenResponse(oAuth2RefreshTokenGrantRequest);
-        accessToken = tokenResponse.getAccessToken();
-        OAuth2AuthorizedClient refreshedOAuth2Client = new OAuth2AuthorizedClient(
-            authorizedClient.getClientRegistration(), authenticationToken.getPrincipal().getName(),
-            accessToken, tokenResponse.getRefreshToken());
-        authorizedClientService.saveAuthorizedClient(refreshedOAuth2Client, authenticationToken);
-        synchronized (clusterMap) {
-          Cluster data = clusterMap.get(authenticatedPrincipalName);
-          if (data != null) {
-            data.reconnectToGemFire(accessToken.getTokenValue());
-          }
-        }
-      }
-      String accessTokenValue = accessToken.getTokenValue();
-      return getClusterWithCredentials(authenticatedPrincipalName, accessTokenValue);
+    // TODO: Find out whether the access token can be null. See the comment in refreshClient().
+    if (freshClient.getAccessToken() == null) {
+      logoutUser(staleClient.getPrincipalName());
+      authentication.setAuthenticated(false);
+      return null;
     }
 
-    return getClusterWithUserNameAndPassword(authentication.getName(), null);
+    return freshClient;
   }
 
-  public Cluster getClusterWithUserNameAndPassword(String userName, String password) {
-    return getClusterWithCredentials(userName, new String[] {userName, password});
+  private OAuth2AuthorizedClient getAuthorizedClient(
+      OAuth2AuthenticationToken authenticationToken) {
+    return authorizedClientService.loadAuthorizedClient(
+        authenticationToken.getAuthorizedClientRegistrationId(), authenticationToken.getName());
   }
 
-  public Cluster getClusterWithCredentials(String username, Object credentials) {
-    synchronized (this.clusterMap) {
-      Cluster data = clusterMap.get(username);
-      if (data == null) {
-        logger.info(resourceBundle.getString("LOG_MSG_CREATE_NEW_THREAD") + " : " + username);
-        data = clusterFactory.create(host, port, username, resourceBundle, this);
-        // Assign name to thread created
-        data.setName(PulseConstants.APP_NAME + "-" + host + ":" + port + ":" + username);
-        data.connectToGemFire(credentials);
-        if (data.isConnectedFlag()) {
-          this.clusterMap.put(username, data);
-        }
-      }
-      return data;
-    }
+  private static OAuth2AuthorizedClient refreshClient(OAuth2AuthorizedClient staleClient) {
+    OAuth2RefreshTokenGrantRequest refreshRequest = new OAuth2RefreshTokenGrantRequest(
+        staleClient.getClientRegistration(),
+        staleClient.getAccessToken(),
+        staleClient.getRefreshToken());
+
+    OAuth2AccessTokenResponse refreshResponse = new DefaultRefreshTokenTokenResponseClient()
+        // As far as I can tell from reading getTokenResponse(), the response will always contain
+        // an access token. So I'm not sure how to know whether the refresh request was granted.
+        // Maybe it throws an exception if the request was denied?
+        .getTokenResponse(refreshRequest);
+
+    return new OAuth2AuthorizedClient(
+        staleClient.getClientRegistration(), staleClient.getPrincipalName(),
+        refreshResponse.getAccessToken(), refreshResponse.getRefreshToken());
   }
 
-  public void logoutUser(String username) {
-    Cluster data = clusterMap.remove(username);
-    if (data != null) {
-      try {
-        data.setStopUpdates(true);
-        data.getJMXConnector().close();
-      } catch (Exception e) {
-        // We're logging out so this can be ignored
-      }
-    }
+  private static boolean isExpired(AbstractOAuth2Token token) {
+    Instant tokenExpiration = token.getExpiresAt();
+    return tokenExpiration != null && now().isAfter(tokenExpiration);
   }
-
-  // This method is used to remove all cluster threads
-  public void removeAllClusters() {
-
-    Iterator<Map.Entry<String, Cluster>> iter = clusterMap.entrySet().iterator();
-
-    while (iter.hasNext()) {
-      Map.Entry<String, Cluster> entry = iter.next();
-      Cluster c = entry.getValue();
-      String clusterKey = entry.getKey();
-      c.stopThread();
-      iter.remove();
-      logger.info("{} : {}", resourceBundle.getString("LOG_MSG_REMOVE_THREAD"), clusterKey);
-    }
-  }
-
-  public ResourceBundle getResourceBundle() {
-    return this.resourceBundle;
-  }
-
-
 }
