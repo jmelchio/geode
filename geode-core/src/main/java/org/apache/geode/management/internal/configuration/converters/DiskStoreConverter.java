db712bbb215636888936505ee77a8ea7e6e09e3e
Joris Melchior <joris.melchior@gmail.com>
2020-06-19 09:07:46 -0400

    WIP - disk-store enpoints for management API

---
+++ geode-core/src/main/java/org/apache/geode/management/internal/configuration/converters/DiskStoreConverter.java
@@ -0,0 +1,76 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.geode.management.internal.configuration.converters;
+
+import java.util.stream.Collectors;
+
+import org.apache.geode.cache.configuration.DiskDirType;
+import org.apache.geode.cache.configuration.DiskStoreType;
+import org.apache.geode.management.configuration.DiskDir;
+import org.apache.geode.management.configuration.DiskStore;
+
+public class DiskStoreConverter extends ConfigurationConverter<DiskStore, DiskStoreType> {
+  @Override
+  protected DiskStore fromNonNullXmlObject(DiskStoreType xmlObject) {
+    DiskStore diskStore = new DiskStore();
+
+    diskStore.setName(xmlObject.getName());
+    diskStore.setId(xmlObject.getId());
+    diskStore.setCompactionThreshold(xmlObject.getCompactionThreshold());
+    diskStore.setDiskUsageCriticalPercentage(xmlObject.getDiskUsageCriticalPercentage());
+    diskStore.setDiskUsageWarningPercentage(xmlObject.getDiskUsageWarningPercentage());
+    diskStore.setMaxOplogSize(xmlObject.getMaxOplogSize());
+    diskStore.setQueueSize(xmlObject.getQueueSize());
+    diskStore.setTimeInterval(xmlObject.getTimeInterval());
+    diskStore.setWriteBufferSize(xmlObject.getWriteBufferSize());
+    diskStore.setDirectories(xmlObject.getDiskDirs().stream().map(diskDirType -> {
+      DiskDir diskDir = new DiskDir();
+      diskDir.setDirSize(diskDirType.getDirSize());
+      diskDir.setName(diskDirType.getContent());
+      return diskDir;
+    }).collect(Collectors.toList()));
+
+    return diskStore;
+  }
+
+  @Override
+  protected DiskStoreType fromNonNullConfigObject(DiskStore configObject) {
+    DiskStoreType diskStoreType = new DiskStoreType();
+
+    diskStoreType.setName(configObject.getName());
+    diskStoreType.setAllowForceCompaction(configObject.isAllowForceCompaction());
+    diskStoreType.setAutoCompact(configObject.isAutoCompact());
+    diskStoreType.setCompactionThreshold(configObject.getCompactionThreshold());
+    diskStoreType.setDiskUsageCriticalPercentage(configObject.getDiskUsageCriticalPercentage());
+    diskStoreType.setDiskUsageWarningPercentage(configObject.getDiskUsageWarningPercentage());
+    diskStoreType.setMaxOplogSize(configObject.getMaxOplogSize());
+    diskStoreType.setQueueSize(configObject.getQueueSize());
+    diskStoreType.setTimeInterval(configObject.getTimeInterval());
+    diskStoreType.setWriteBufferSize(configObject.getWriteBufferSize());
+    diskStoreType.setDiskDirs(configObject.getDirectories().stream().map(diskDir -> {
+      DiskDirType diskDirType = new DiskDirType();
+      diskDirType.setContent(diskDir.getName());
+      diskDirType.setDirSize(diskDir.getDirSize());
+      return diskDirType;
+    }).collect(Collectors.toList()));
+
+    return diskStoreType;
+  }
+}
---
+++ geode-management/src/main/java/org/apache/geode/management/configuration/DiskStore.java
@@ -0,0 +1,144 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.geode.management.configuration;
+
+import java.util.List;
+
+import org.apache.geode.management.runtime.DiskStoreInfo;
+
+public class DiskStore extends GroupableConfiguration<DiskStoreInfo> {
+  public static final String DISK_STORE_CONFIG_ENDPOINT = "/diskstores";
+
+  private String id;
+  private String name;
+  private String compactionThreshold;
+  private String diskUsageCriticalPercentage;
+  private String diskUsageWarningPercentage;
+  private String maxOplogSize;
+  private String queueSize;
+  private String timeInterval;
+  private String writeBufferSize;
+  private List<DiskDir> directories;
+  private boolean allowForceCompaction;
+  private boolean autoCompact;
+
+  public boolean isAutoCompact() {
+    return autoCompact;
+  }
+
+  public void setAutoCompact(boolean autoCompact) {
+    this.autoCompact = autoCompact;
+  }
+
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public List<DiskDir> getDirectories() {
+    return directories;
+  }
+
+  public void setDirectories(List<DiskDir> directories) {
+    this.directories = directories;
+  }
+
+  @Override
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getCompactionThreshold() {
+    return compactionThreshold;
+  }
+
+  public void setCompactionThreshold(String compactionThreshold) {
+    this.compactionThreshold = compactionThreshold;
+  }
+
+  public String getDiskUsageCriticalPercentage() {
+    return diskUsageCriticalPercentage;
+  }
+
+  public void setDiskUsageCriticalPercentage(String diskUsageCriticalPercentage) {
+    this.diskUsageCriticalPercentage = diskUsageCriticalPercentage;
+  }
+
+  public String getDiskUsageWarningPercentage() {
+    return diskUsageWarningPercentage;
+  }
+
+  public void setDiskUsageWarningPercentage(String diskUsageWarningPercentage) {
+    this.diskUsageWarningPercentage = diskUsageWarningPercentage;
+  }
+
+  public String getMaxOplogSize() {
+    return maxOplogSize;
+  }
+
+  public void setMaxOplogSize(String maxOplogSize) {
+    this.maxOplogSize = maxOplogSize;
+  }
+
+  public String getQueueSize() {
+    return queueSize;
+  }
+
+  public void setQueueSize(String queueSize) {
+    this.queueSize = queueSize;
+  }
+
+  public String getTimeInterval() {
+    return timeInterval;
+  }
+
+  public void setTimeInterval(String timeInterval) {
+    this.timeInterval = timeInterval;
+  }
+
+  public String getWriteBufferSize() {
+    return writeBufferSize;
+  }
+
+  public void setWriteBufferSize(String writeBufferSize) {
+    this.writeBufferSize = writeBufferSize;
+  }
+
+  public boolean isAllowForceCompaction() {
+    return allowForceCompaction;
+  }
+
+  public void setAllowForceCompaction(boolean allowForceCompaction) {
+    this.allowForceCompaction = allowForceCompaction;
+  }
+
+  @Override
+  public Links getLinks() {
+    return new Links(getId(), DISK_STORE_CONFIG_ENDPOINT);
+  }
+}
